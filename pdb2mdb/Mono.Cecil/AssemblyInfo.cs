12bb9badeaaeddf9882cfe6f141a84c6978dd7fe
bodong <dbdongbo@vip.qq.com>
2021-10-05 09:01:26 +0800

    read me and re build.

--- pdb2mdb/Mono.Cecil/AssemblyInfo.cs
+++ pdb2mdb/Mono.Cecil/AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle (Consts.AssemblyName)]
+//[assembly: AssemblyTitle (Consts.AssemblyName)]
 
-[assembly: Guid ("fd225bb4-fa53-44b2-a6db-85f5e48dcb54")]
+//[assembly: Guid ("fd225bb4-fa53-44b2-a6db-85f5e48dcb54")]
 
 [assembly: InternalsVisibleTo ("Mono.Cecil.Tests, PublicKey=" + Consts.PublicKey)]
 [assembly: InternalsVisibleTo ("Mono.Cecil.Pdb, PublicKey=" + Consts.PublicKey)]
